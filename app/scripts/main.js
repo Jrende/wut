f206db54276a02f26a1956605f59f6032446af0b
jrende <johan@rende.se>
2015-04-19 22:03:03 +0200

    Draw quad

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,28 @@
-var context = require('./gl.js')(document.querySelector("#canvas"));
+var context = require('./gl.js');
+context(document.querySelector("#canvas"));
+var gl = context.gl;
 var Shader = require('./shader.js')
-Promise.all(["test", "other"].map(Shader.createShader)).then((shaders) => {
+var VertexArray = require('./vertexbuffer.js');
+Promise.all(["test"].map(Shader.createShader)).then((shaders) => {
+  gl.clearColor(0, 0, 0, 1);
+  gl.disable(gl.CULL_FACE);
   var s = shaders[0];
-  console.log("all shaders created");
+  var vertexArray = new VertexArray(
+    [1.0, 1.0,
+    -1.0, 1.0,
+    -1.0, -1.0,
+    1.0, -1.0],
+    [0, 1, 2,
+     0, 2, 3],
+    [2]);
+  s.use();
+  /*
+  s.r = 1.0;
+  s.g = 0.0;
+  s.b = 1.0;
+  s.alpha = 1.0;
+  */
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  vertexArray.bind();
+  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
 });
