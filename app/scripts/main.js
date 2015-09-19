6c50f7c4fb36a614b529833a96c331dbf3dab2d9
jrende <johan@rende.se>
2015-09-19 20:39:14 +0200

    Use RAF for draw loop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,15 +4,11 @@ var gl = context.gl;
 var Shader = require('./shader.js')
 var VertexArray = require('./vertexbuffer.js');
 var Emitter = require('./emitter.js');
-var emitter = new Emitter(5).spread(360).acceleration(5);
-for(var i = 0; i < 10; i++) {
-	emitter.tick();
-}
+var Renderer = require('./renderer.js');
+var emitter = new Emitter(5).spread(45).acceleration(0.0).speed(0.001).pos([0, 0, 0]);
 window.emitter = emitter;
 
-Promise.all(["test"].map(Shader.createShader)).then((shaders) => {
-  gl.clearColor(0, 0, 0, 1);
-  gl.disable(gl.CULL_FACE);
+Promise.all(["test", "particleShader"].map(Shader.createShader)).then((shaders) => {
   var s = shaders[0];
   var vertexArray = new VertexArray(
     [1.0, 1.0,
@@ -22,12 +18,23 @@ Promise.all(["test"].map(Shader.createShader)).then((shaders) => {
     [0, 1, 2,
      0, 2, 3],
     [2]);
-  s.use();
-  s.uniforms.r = 0.0;
-  s.uniforms.g = 0.5;
-  s.uniforms.b = 1.0;
-  s.uniforms.alpha = 1.0;
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  vertexArray.bind();
-  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+  function draw() {
+    emitter.tick();
+
+    gl.clearColor(0, 0, 0, 1);
+    gl.disable(gl.CULL_FACE);
+    s.use();
+    s.uniforms.r = 0.0;
+    s.uniforms.g = 0.5;
+    s.uniforms.b = 1.0;
+    s.uniforms.alpha = 1.0;
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    vertexArray.bind();
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+    Renderer.renderParticles(emitter.particles, shaders[1]);
+    window.requestAnimationFrame(draw);
+  }
+  draw();
 });
