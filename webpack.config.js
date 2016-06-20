e42a8d0723d6c49c63910feba9854c5f65491150
Johan Rende <johan@rende.se>
2016-06-20 21:05:23 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var index = path.join(__dirname, './app/scripts/main.js');
+
+module.exports = {
+  entry: [
+    index
+  ],
+
+  devtool: 'source-map',
+  output: {
+    path: 'dist',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    /*
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      mangle: true,
+      compress: {
+        unsafe: true
+      }
+    }),
+    */
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'app/index.html',
+      inject: true
+    }),
+    new ExtractTextPlugin('style.css')
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      {
+        test: /\.(frag|vert)/,
+        loaders: ['raw-loader'],
+      },
+      {
+        test: /\.css?$/,
+        loaders: ['null'],
+        include: __dirname
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+      }
+    ]
+  }
+};
